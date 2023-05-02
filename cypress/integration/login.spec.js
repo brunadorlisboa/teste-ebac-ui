fd788653ec68f48364502b14a00cb425810c0d8f
Bruna Lisboa <brunadornelaslisboa@gmail.com>
2023-05-02 16:59:51 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,37 +1,59 @@
 /// <reference types="cypress" />
-
-context('Funcionalidade Login', () =>{
+const perfil = require('../fixtures/perfil.json')
+context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
-    
+
     afterEach(() => {
         cy.screenshot()
     });
 
     it('Deve fazer login com sucesso', () => {
- cy.get('#username').type('aluno_ebac@teste.com')
- cy.get('#password').type('teste@teste.com')
- cy.get('.woocommerce-form > .button').click()
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
 
- cy.get('.page-title').should('contain' , 'Minha conta')
- cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, Aluno (não é Aluno? Sair)')
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno? Sair)')
     })
+
+    it('Deve fazer login com sucesso - usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+
+
+    });
+
+    it('Deve fazer login com sucesso - usando fixtures') , () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.page-title').should('contain' , 'Minha conta')
+        })
+    }
+    
+       
+
+    
 
     it('deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
         cy.get('#username').type('aluno_eb@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
-           })
+    })
 
-           it('deve exibir uma mensagem de erro ao inserir senha invalida', () => {
-            cy.get('#username').type('aluno_ebac@teste.com')
-            cy.get('#password').type('teste@teste')
-            cy.get('.woocommerce-form > .button').click()
-            cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
-           
-        })
+    it('deve exibir uma mensagem de erro ao inserir senha invalida', () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+
+    })
 })
 
